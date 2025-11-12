cf96dedaeb9c361bad6d4fbbd683979e0b7dc6d4
Strike32GT <roberto.mas@tecsup.edu.pe>
2025-11-12 03:28:42 -0500

    Primer Commir

---
+++ src/main/java/com/example/simulacion_pasarela_pagos/SimulacionPasarelaPagosApplication.java
@@ -0,0 +1,13 @@
+package com.example.simulacion_pasarela_pagos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulacionPasarelaPagosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimulacionPasarelaPagosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simulacion_pasarela_pagos/SimulacionPasarelaPagosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simulacion_pasarela_pagos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulacionPasarelaPagosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
